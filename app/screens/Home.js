53716f462de388af5c43d4afd64d0c76d04c6ffb
rghossi <rafael.ghossi@gmail.com>
2017-10-24 23:08:49 -0200

    Initial commit

---
+++ app/screens/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { Container } from '../components/Container';
+
+export default () => (
+  <Container>
+    <Text>Hello from Home Screen</Text>
+  </Container>
+);
